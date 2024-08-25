08c4c69033bc4c3bdd94163b14077a9b67e8f627
LeandroAgristola <leandro.agristola@gmail.com>
2024-08-24 23:22:09 -0300

    elimino proyecto

--- Proyectoweb/autenticacion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import VRegistro, cerrar_sesion, logear
-
-urlpatterns = [
-    path('', VRegistro.as_view(), name="Autenticacion"),  # Vista basada en clase, correcto usar as_view()
-    path('cerrar_sesion', cerrar_sesion, name="cerrar_sesion"),  # Vista basada en función, eliminar as_view()
-    path('logear', logear, name="logear"),  # Vista basada en función, eliminar as_view()
-]
--- Proyectoweb/contacto/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class FormularioContacto(forms.Form):
-    nombre=forms.CharField(label="Nombre",required=True,max_length=50)
-    email=forms.EmailField(label="email",required=True,max_length=50)
-    contenido=forms.CharField(label="contenido",required=False,max_length=150,widget=forms.Textarea)
-
