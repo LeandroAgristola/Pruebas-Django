3114ae1b0858ae2255d5e752819279c0c3f0b93f
LeandroAgristola <leandro.agristola@gmail.com>
2024-09-16 19:30:33 -0300

    configurando proyecto para produccion

--- realstate/settings.py
+++ realstate/settings.py
@@ -24,12 +24,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+h53uwczmidm8s(=0^68s5#@pbrzquz((=c_b+5j+1@x2z0zl%'
+# *CONFIGURACION PARA DEPLOY EN RENDER*
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# *CONFIGURACION PARA DEPLOY EN RENDER*
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+# *CONFIGURACION PARA DEPLOY EN RENDER*
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
