a720ee8801edf63666474fead14aaeeddabfdf92
LeandroAgristola <leandro.agristola@gmail.com>
2024-09-18 22:42:05 -0300

    corrigiendo errores 6

--- realstate/settings.py
+++ realstate/settings.py
@@ -147,7 +147,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 
