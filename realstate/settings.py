296305748705fbbda3752179c30397d7d1524dc4
LeandroAgristola <145815272+LeandroAgristola@users.noreply.github.com>
2024-10-07 16:08:44 +0000

    configuracndo cloud google 04

--- realstate/settings.py
+++ realstate/settings.py
@@ -157,11 +157,7 @@ if not DEBUG:
     # Aquí podrías tener otra configuración relacionada con producción
     pass  # Este bloque puede quedar vacío o puedes agregar configuraciones específicas para producción
 
-
-if not DEBUG:
-    # Esto permite que Django sirva archivos multimedia en producción (si usas servicios como AWS S3, este bloque cambiará)
-    MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+th.join(BASE_DIR, 'media')
     
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -213,10 +209,13 @@ APPEND_SLASH = True
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')  # Usa la variable de entorno para el nombre del bucket
 
-# Cargar las credenciales
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-    os.path.join(BASE_DIR, os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
-)
+# Configuración de Google Cloud Storage
+GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')  # Usa la variable de entorno para el nombre del bucket
 
 # URL pública de los archivos subidos
-MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
+if not DEBUG:
+    MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
+else:
+    MEDIA_URL = '/media/'  # Cambia esta línea si deseas manejar archivos multimedia localmente
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Esto se mantiene, pero no se utilizará en producción
