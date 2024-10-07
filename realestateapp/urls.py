ea2f1d0f85260b5ceae9981fd13c18749f675153
LeandroAgristola <leandro.agristola@gmail.com>
2024-10-07 19:44:18 -0300

    agregamos un test del bucket

--- realestateapp/urls.py
+++ realestateapp/urls.py
@@ -3,12 +3,15 @@ from realestateapp import views
 from django.conf import settings 
 from django.conf.urls.static import static
 
+
+from .views import test_gcs_access  # Asegúrate de importar la función
+
 urlpatterns = [
     path('',views.home, name="home"),
     path('mobileViviendas/', views.mobileViviendas, name='mobileViviendas'),
     path('mobileEdificios/', views.mobileEdificios, name='mobileEdificios'),
     path('mobileIndustrias/', views.mobileIndustrias, name='mobileIndustrias'),
-]
+    path('test-gcs/', test_gcs_access, name='test_gcs'),
 
 if settings.DEBUG: 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
