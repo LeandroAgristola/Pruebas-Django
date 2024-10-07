7191e736e363b8b505b6fe8cc61a20a643c2daef
LeandroAgristola <145815272+LeandroAgristola@users.noreply.github.com>
2024-10-07 15:07:28 +0000

    configurando google cloud

--- realstate/settings.py
+++ realstate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from google.oauth2 import service_account
 
 import os
 # *CONFIGURACION PARA DEPLOY EN RENDER*
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'realestateapp',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -155,10 +157,10 @@ if not DEBUG:
     # Aquí podrías tener otra configuración relacionada con producción
     pass  # Este bloque puede quedar vacío o puedes agregar configuraciones específicas para producción
 
-if not DEBUG:
-    # Esto permite que Django sirva archivos multimedia en producción (si usas servicios como AWS S3, este bloque cambiará)
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Esto permite que Django sirva archivos multimedia en producción (si usas servicios como AWS S3, este bloque cambiará)
+MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media') No es necesario cuando utilizamos google cloud
     
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -205,3 +207,15 @@ LOGIN_URL = '/authentication/logear/'  # Define la URL a la que se redirige si n
 LOGIN_REDIRECT_URL = 'home'  # Redirige a 'home' después de iniciar sesión
 
 APPEND_SLASH = True
+
+# Configuración de Google Cloud Storage
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = 'realestatemedia'
+
+# Cargar las credenciales
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+)
+
+# URL pública de los archivos subidos
+MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
