00ec3c1cb57c0d969787173c9440de6392a9990a
LeandroAgristola <145815272+LeandroAgristola@users.noreply.github.com>
2024-10-10 14:20:59 +0000

    Intentando una nueva configuracion para carga de archivos de media 02

--- realstate/settings.py
+++ realstate/settings.py
@@ -211,13 +211,14 @@ GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 )
 
-DEFAULT_FILE_STORAGE = 'DJANGO-PROYECT001.gcloud.GoogleCloudMediaFileStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
 GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')  # Usa la variable de entorno para el nombre del bucket
 GS_BUCKET_ID = os.environ.get('GS_PROJECT_ID') # Usa la variable de entorno para el ID del bucket
 
+GS_FILE_OVERWRITE = False  # Evitar sobreescribir archivos
+GS_DEFAULT_ACL = None  # Evitar que se apliquen ACL predeterminadas
+
 MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_BUCKET_NAME)
 MEDIA_ROOT= "media/"
 UPLOAD_ROOT = 'media/uploads/'
-
-#DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
